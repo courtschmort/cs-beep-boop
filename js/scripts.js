43f622feed51d5df6f1f9bb398d24d9e83aafe87
Courtney Schild <courtneyschild@gmail.com>
2019-10-04 22:48:46 -0700

    revise JavaScript to replace array elements with strings

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,63 @@
-// business logic
-// function newFunction(numbers) {
-//
-// };
+//business logic
+var functionNumbers = function(number) {
+  var numbers = [];
+  for (index = 0; index <= number; index += 1) {
+    if (index.toString().includes("3")) {
+      numbers.push(" I'm sorry Dave. I'm afraid I can't do that!");
+    } else if (index.toString().includes("2")) {
+      numbers.push(" Boop!");
+    } else if (index.toString().includes("1")) {
+      numbers.push(" Beep!");
+    } else {
+      numbers.push(" " + index);
+    }
+  }
+  return numbers; //return the final results
+}
 
-// user interface logic
+///user interface logic
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    let number = parseInt($("input#number").val());
 
-    let numbers = [];
-    for (index = 0; index <= number; index += 1) {
-      numbers.push(index);
-    };
+    var number = parseInt($("input#number").val());
+    var result = functionNumbers(number);
 
-    // numbers.forEach(function(number) {
-    //   if ((number.toString()).includes("3")) {
-    //     numbers.push(" " + "Hi!");
-    //   };
-    // });
-
-    // let result = newFunction(number);
-
-    // if (number === "") {
-    //   alert("Please enter a number.");
-    // };
-
-    $(".array").text(numbers);
+    $(".array").text(result);
     $("#result").show();
-    });
+
   });
+});
+
+// // business logic
+// // function newFunction(numbers) {
+// //
+// // };
+//
+// // user interface logic
+// $(document).ready(function() {
+//   $("form").submit(function(event) {
+//     event.preventDefault();
+//     let number = parseInt($("input#number").val());
+//
+//     let numbers = [];
+//     for (index = 0; index <= number; index += 1) {
+//       numbers.push(index);
+//     };
+//
+//     // numbers.forEach(function(number) {
+//     //   if ((number.toString()).includes("3")) {
+//     //     numbers.push(" " + "Hi!");
+//     //   };
+//     // });
+//
+//     // let result = newFunction(number);
+//
+//     // if (number === "") {
+//     //   alert("Please enter a number.");
+//     // };
+//
+//     $(".array").text(numbers);
+//     $("#result").show();
+//     });
+//   });
